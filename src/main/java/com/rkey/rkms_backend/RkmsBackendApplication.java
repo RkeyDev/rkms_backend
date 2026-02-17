a2bbab366d6f3e06ee66c1200f803d090707e3e7
RkeyDev <roei1576@gmail.com>
2026-02-17 22:06:27 +0200

    init commit

---
+++ src/main/java/com/rkey/rkms_backend/RkmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rkey.rkms_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RkmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RkmsBackendApplication.class, args);
+	}
+
+}
