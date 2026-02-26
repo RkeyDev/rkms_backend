1fca27655dcb679543d79567f9154fa9d5ab3a54
RkeyDev <roei1576@gmail.com>
2026-02-26 23:12:10 +0200

    Add H2 database dependency and configure test properties for in-memory testing

--- src/test/java/com/rkey/rkms_backend/RkmsBackendApplicationTests.java
+++ src/test/java/com/rkey/rkms_backend/RkmsBackendApplicationTests.java
@@ -2,12 +2,21 @@ package com.rkey.rkms_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;MODE=PostgreSQL",
+    "spring.datasource.driverClassName=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+    "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class RkmsBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // If this reaches here, the context started successfully.
+    }
 }
