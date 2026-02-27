fc9b417d8b75aa890fd3ad183ef9e0a0c1ae89c8
RkeyDev <roei1576@gmail.com>
2026-02-27 13:12:36 +0200

    Refactor RkmsBackendApplicationTests to remove H2 configuration and utilize real SecurityConfig for PasswordEncoder

--- src/test/java/com/rkey/rkms_backend/RkmsBackendApplicationTests.java
+++ src/test/java/com/rkey/rkms_backend/RkmsBackendApplicationTests.java
@@ -2,33 +2,14 @@ package com.rkey.rkms_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-    "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;MODE=PostgreSQL",
-    "spring.datasource.driverClassName=org.h2.Driver",
-    "spring.datasource.username=sa",
-    "spring.datasource.password=",
-    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
-    "spring.jpa.hibernate.ddl-auto=create-drop"
-})
 class RkmsBackendApplicationTests {
-
-    @TestConfiguration
-    static class TestConfig {
-        @Bean
-        public PasswordEncoder passwordEncoder() {
-            return new BCryptPasswordEncoder();
-        }
-    }
 
     @Test
     void contextLoads() {
-        // Now it will find the PasswordEncoder bean defined above!
+        // This test will now pass because it will use the 
+        // PasswordEncoder from your real SecurityConfig
     }
+
 }
