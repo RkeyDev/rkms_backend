87feba1414989b4af3eddd902931df8047b633f7
RkeyDev <roei1576@gmail.com>
2026-03-09 19:25:13 +0200

    Update UserRegistrationDTO in AuthServiceTests to include company field for registration

--- src/test/java/com/rkey/rkms_backend/modules/auth/service/AuthServiceTests.java
+++ src/test/java/com/rkey/rkms_backend/modules/auth/service/AuthServiceTests.java
@@ -1,17 +1,22 @@
 package com.rkey.rkms_backend.modules.auth.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -39,7 +44,7 @@ public class AuthServiceTests {
     @Test
     @DisplayName("Should save users in the DB when registering.")
     void registerUser_NormalScenario() {
-        UserRegistrationDTO userDto = new UserRegistrationDTO("Roei", "Kleiner", "roei1576@gmail.com", "roeiPassword", "roeiPassword");
+        UserRegistrationDTO userDto = new UserRegistrationDTO("Roei", "Kleiner", "roei1576@gmail.com", "rkey", "roeiPassword", "roeiPassword");
         when(passwordEncoder.encode(userDto.password())).thenReturn("hashedPassword123");
 
         boolean isRegistered = authService.registerUser(userDto);
